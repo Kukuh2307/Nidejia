0789938e2cb0dbac48753e09b4d34adfc957d2f8
kukuh2307 <kukuhagung12@gmail.com>
2024-04-12 22:42:16 +0700

    finish: 2.1 listing resource add model and migration

---
+++ app/Models/listing.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class listing extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "title", "slug", "description", "address", "sqft", "wifi_speed", "max_person", "price_per_day", "price_per_week", "price_per_month", "price_per_year", "attachments", "full_support_available", "gym_area_available", "mini_cafe_available", "cinema_available"
+    ];
+
+    protected $casts = [
+        "attachments" => "array"
+    ];
+}
