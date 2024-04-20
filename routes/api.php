9e1bf278c8d3fb430d09c5e259701159c55c6bed
kukuh2307 <kukuhagung12@gmail.com>
2024-04-20 23:09:53 +0700

    finish: 5.6 Auth Endpoint detail user login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json([
+        'success' => true,
+        'message' => 'Detail logi user',
+        'data' => $request->user()
+    ]);
 });
 require __DIR__ . '/auth.php';
